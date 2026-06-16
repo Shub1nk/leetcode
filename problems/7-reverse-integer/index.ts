8668be4bde3c34ca4adaec1246eeef214b1497e3
Шубин Николай <nashubin@alfabank.ru>
2026-06-16 12:53:21 +0500

    refactor: 7-reverse-integer

--- problems/7-reverse-integer/index.ts
+++ problems/7-reverse-integer/index.ts
@@ -1,3 +1,7 @@
 import { mathReverseInteger } from "../../shared/math-reverse-integer";
+import { isInt32 } from "../../shared/is-int32";
 
-export const reverseInteger = (int: number) => mathReverseInteger(int);
+export const reverseInteger = (int: number): number => {
+  const reversed = mathReverseInteger(int);
+  return isInt32(reversed) ? reversed : 0;
+};
