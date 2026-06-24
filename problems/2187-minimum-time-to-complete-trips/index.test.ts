7aea8ee1ccad9deed501d741807e680697fdfd52
Шубин Николай <nashubin@alfabank.ru>
2026-06-24 17:31:31 +0500

    problem: add 2187 Minimun Time to Complete Trips

---
+++ problems/2187-minimum-time-to-complete-trips/index.test.ts
@@ -0,0 +1,12 @@
+import { minimumTimeToCompleteTrips } from ".";
+import { bigTimes } from './big-mock'
+
+describe("minimumTimeToCompleteTrips", () => {
+  it("Должен вернуть минимальное время для вычисления путешествия", () => {
+    expect(minimumTimeToCompleteTrips([1, 2, 3], 5)).toEqual(3);
+    expect(minimumTimeToCompleteTrips([2], 1)).toEqual(2);
+  });
+  it("Должен вернуть минимальное время для большого набора данных с бинарным поиском", () => {
+    expect(minimumTimeToCompleteTrips(bigTimes, 9765277)).toEqual(34766088);
+  })
+});
---
+++ problems/2187-minimum-time-to-complete-trips/index.ts
@@ -0,0 +1,28 @@
+const getSumTrips = (trips: number[]) => trips.reduce((a, b) => a + b);
+
+const getMid = (left: number, right: number) => left + Math.floor((right-left)/2);
+
+export const minimumTimeToCompleteTrips = (time: number[], totalTrips: number): number => {
+  let left = Math.min(...time);
+  let right = left * totalTrips;
+
+  while (left < right) {
+    let mid = getMid(left, right);
+
+    const trips = time.map((t) => Math.floor(mid / t));
+    const sumTrip = getSumTrips(trips);
+
+    if (sumTrip >= totalTrips) {
+      right = mid;
+      mid = getMid(left, right);
+    }
+
+    if (sumTrip < totalTrips) {
+      left = mid + 1;
+      mid = getMid(left, right);
+    }
+  }
+
+  return left;
+};
+
