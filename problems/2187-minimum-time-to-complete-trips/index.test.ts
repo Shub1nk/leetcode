f77ec21e2d523c35d7948189862d78ea671c22bd
Шубин Николай <nashubin@alfabank.ru>
2026-06-25 22:17:03 +0500

    chore: add equal-times test to 2187

--- problems/2187-minimum-time-to-complete-trips/index.test.ts
+++ problems/2187-minimum-time-to-complete-trips/index.test.ts
@@ -6,6 +6,10 @@ describe("minimumTimeToCompleteTrips", () => {
     expect(minimumTimeToCompleteTrips([1, 2, 3], 5)).toEqual(3);
     expect(minimumTimeToCompleteTrips([2], 1)).toEqual(2);
   });
+  it("Одинаковые времена у всех машин", () => {
+    expect(minimumTimeToCompleteTrips([5, 5, 5], 3)).toEqual(5);
+    expect(minimumTimeToCompleteTrips([5, 5, 5], 4)).toEqual(10);
+  });
   it("Должен вернуть минимальное время для большого набора данных с бинарным поиском", () => {
     expect(minimumTimeToCompleteTrips(bigTimes, 9765277)).toEqual(34766088);
   });
