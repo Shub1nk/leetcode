7abf44b90f451fd30929ca9e9c10edc676be5a50
Шубин Николай <nashubin@alfabank.ru>
2026-06-25 22:16:57 +0500

    refactor: unify two-sum docs and test style

--- problems/1-two-sum/index.test.ts
+++ problems/1-two-sum/index.test.ts
@@ -1,17 +1,17 @@
-import { twoSum } from "./index";
+import { twoSum } from ".";
 
 describe("twoSum", () => {
-  test("пример 1: [2, 7, 11, 15], target = 9", () => {
+  it("пример 1: [2, 7, 11, 15], target = 9", () => {
     const expected = [0, 1];
     expect(twoSum([2, 7, 11, 15], 9)).toEqual(expected);
   });
 
-  test("пример 2: [3, 2, 4], target = 6", () => {
+  it("пример 2: [3, 2, 4], target = 6", () => {
     const expected = [1, 2];
     expect(twoSum([3, 2, 4], 6)).toEqual(expected);
   });
 
-  test("пример 3: [3, 3], target = 6", () => {
+  it("пример 3: [3, 3], target = 6", () => {
     const expected = [0, 1];
     expect(twoSum([3, 3], 6)).toEqual(expected);
   });
