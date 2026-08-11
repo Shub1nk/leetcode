8d4755aebb90c3c02eae038d0f0b5a76c0144704
Shub1nk <shub1nk@yandex.ru>
2026-08-11 13:49:17 +0000

    chore: поправить грамматическую ошибку в тестах 875

--- problems/875-koko-eating-bananas/__tests__/index.test.ts
+++ problems/875-koko-eating-bananas/__tests__/index.test.ts
@@ -1,7 +1,7 @@
 import { minEatingSpeed } from "..";
 import { cases } from "./cases";
 
-describe.each([["beenary search", minEatingSpeed]])("minEatingSpeed  · %s", (_name, solve) => {
+describe.each([["binary search", minEatingSpeed]])("minEatingSpeed  · %s", (_name, solve) => {
   it.each(cases)("$name", ({ data, expected }) => {
     const { piles, h } = data;
     expect(solve(piles, h)).toBe(expected);
